f54728ff2799c2f68ad667237e0ac0bc261b16e7
Denis2389 <bereznyakdenis2003@gmail.com>
2024-10-30 12:38:09 +0200

    add components

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,15 @@
+export const TodoList: React.FC = () => {
+
+    const [todos, setTodos]
+
+    return (
+        <div>
+            <h1>
+                ToDo List
+            </h1>
+            <ul>
+
+            </ul>
+        </div>
+    )
+}
