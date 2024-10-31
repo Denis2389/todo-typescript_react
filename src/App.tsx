3b1cf359262c25db6524c78ecd3b59604dea2516
Denis2389 <bereznyakdenis2003@gmail.com>
2024-10-31 14:22:21 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import { Task } from './Task'
+import { useState } from 'react'
 
 const App: React.FC = () => {
+  const [task, setTask] = useState<Task[]>([]);
+
   return (
     <div>
-      <h1>Currency Converter</h1>
+      <h1>To-Do List</h1>
     </div>
   )
 }
